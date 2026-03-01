dd944c43521998658e5c69bad6f23b23c3335068
TanKimzeg <1964375900@qq.com>
2026-03-01 11:52:21 +0800

    fix: 不应对用户口令序列化

--- src/main/java/top/tankimzeg/editorial_system/entity/User.java
+++ src/main/java/top/tankimzeg/editorial_system/entity/User.java
@@ -1,5 +1,6 @@
 package top.tankimzeg.editorial_system.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.Data;
 import org.hibernate.annotations.CreationTimestamp;
@@ -19,6 +20,7 @@ public class User {
     @Column(unique = true, nullable = false)
     private String username;
 
+    @JsonIgnore
     @Column(nullable = false)
     private String password;
 
